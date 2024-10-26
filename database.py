daf1a4644ef1923011b2554cb6b5661428714861
jasminetech0 <jiebing.chen.2020@globis.ac.jp>
2024-10-26 12:33:56 +0900

    FASTAPIのサーバーにSSH証明書を設定

--- database.py
+++ database.py
@@ -9,8 +9,9 @@ from dotenv import load_dotenv
 # .envから環境変数を読み込む
 load_dotenv()
 
-# データベース接続の設定
-DATABASE_URL = f"mysql+mysqlconnector://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DATABASE')}"
+# データベース接続の設定（SSL証明書を指定）
+DATABASE_URL = f"mysql+mysqlconnector://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DATABASE')}?ssl_ca=/home/site/certificates/DigiCertGlobalRootCA.crt.pem"
+
 
 # SQLAlchemyのエンジンを作成
 engine = create_engine(DATABASE_URL)
